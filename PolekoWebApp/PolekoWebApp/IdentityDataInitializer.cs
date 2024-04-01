0e06bbec7628a7b35b7c2d1c2e0601716f89738d
schmatteo <michal.skrzyszowski@hotmail.com>
2024-04-01 20:41:39 +0200

    Initial commit

---
+++ PolekoWebApp/PolekoWebApp/IdentityDataInitializer.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PolekoWebApp.Data;
+
+public static class IdentityDataInitializer
+{
+    private static readonly string[] Roles = ["Admin", "User"];
+
+    public static async Task SeedRoles(RoleManager<IdentityRole> roleManager)
+    {
+        foreach (var roleName in Roles)
+        {
+            if (await roleManager.RoleExistsAsync(roleName)) continue;
+            var role = new IdentityRole {Name=roleName};
+            await roleManager.CreateAsync(role);
+        }
+    }
+
+    public static async Task RemoveUsers(UserManager<ApplicationUser> userManager)
+    {
+        var users = userManager.Users.ToList();
+        foreach (var user in users)
+        {
+            await userManager.DeleteAsync(user);
+        }
+    }
+}
---
+++ PolekoWebApp/PolekoWebApp/Program.cs
@@ -0,0 +1,79 @@
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PolekoWebApp.Components;
+using PolekoWebApp.Components.Account;
+using PolekoWebApp.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents(options => options.DetailedErrors = builder.Environment.IsProduction())
+    .AddInteractiveServerComponents();
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<IdentityUserAccessor>();
+builder.Services.AddScoped<IdentityRedirectManager>();
+builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+    })
+    .AddIdentityCookies();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+                       throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentityCore<ApplicationUser>()
+    .AddRoles<IdentityRole>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.User.RequireUniqueEmail = true;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+using var scope = app.Services.CreateScope();
+var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+await IdentityDataInitializer.SeedRoles(roleManager);
+// await IdentityDataInitializer.RemoveUsers(userManager);
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+// Add additional endpoints required by the Identity /Account Razor components.
+app.MapAdditionalIdentityEndpoints();
+
+app.Run();
