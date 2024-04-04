2fa1399dcd8232b24e37fcbbc55a9b931a2dafca
schmatteo <michal.skrzyszowski@hotmail.com>
2024-04-04 19:36:54 +0200

    Fix data transfer from probe

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -12,13 +12,13 @@ unsigned long currentTime = millis();
 unsigned long previousTime = 0;
 const long timeoutTime = 2000;
 
-// put function declarations here:
-int myFunction(int, int);
+String receivedMessage;
+
+HardwareSerial SensorSerial(2);
+
 void initialWifiSetup();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
   // WiFi.mode(WIFI_AP);
   // WiFi.softAP("smallboard", "12345678");
   WiFi.config(localIP, gateway, subnet);
@@ -27,47 +27,80 @@ void setup() {
     delay(500);
   }
   server.begin();
+  Serial.begin(9600);
+  Serial.println("looool");
+  SensorSerial.begin(19200, SERIAL_8N1, 16, 17);
+  SensorSerial.println('\n');
+  Serial.println(SensorSerial.baudRate());
+    // Serial1.begin(19200, SERIAL_8N1);
+    // Serial1.println('\n');
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  WiFiClient client = server.available();
+//   WiFiClient client = server.available();
 
-  if (client) {
-    currentTime = millis();
-    previousTime = currentTime;
-    String currentLine = "";
-    while(client.connected()) {
-      currentTime = millis();
-      if (client.available()) {
-          char c = client.read();
-          header += c;
-          if (c == '\n') {
-            if (currentLine.length() == 0) {
-              client.println("HTTP/1.1 200 OK");
-              client.println("Content-type:text/html");
-              client.println("Connection: close");
-              client.println();
+//   if (client) {
+//     currentTime = millis();
+//     previousTime = currentTime;
+//     String currentLine = "";
+//     while(client.connected()) {
+//       currentTime = millis();
+//       if (client.available()) {
+//           char c = client.read();
+//           header += c;
+//           if (c == '\n') {
+//             if (currentLine.length() == 0) {
+//               client.println("HTTP/1.1 200 OK");
+//               client.println("Content-type:text/html");
+//               client.println("Connection: close");
+//               client.println();
 
-              if (header.indexOf("GET /lol") >= 0) {
-                client.println("<html><body>xdddd</body></html>");
-              }
-              client.println();
-              break;
-            } else {
-              currentLine = "";
-            }
-          } else if (c != '\r') {
-            currentLine += c;
-          }
-      }
+//               if (header.indexOf("GET /lol") >= 0) {
+//                 client.println("<html><body>xdddd</body></html>");
+//               }
+//               client.println();
+//               break;
+//             } else {
+//               currentLine = "";
+//             }
+//           } else if (c != '\r') {
+//             currentLine += c;
+//           }
+//       }
+//     }
+//     header = "";
+//     client.stop();
+//   }
+
+    SensorSerial.println("{F99RDD}\r\n");
+    delay(2000); // Adjust delay based on probe response time
+
+    unsigned long startTime = millis();
+    char receivedChar;
+    String receivedMessage = "";
+
+    // while (SensorSerial.available() > 0 && (millis() - startTime) < 1000) { // Limit wait time
+    //     // receivedChar = (char)SensorSerial.read(); // Cast to char
+    //     // if (receivedChar == '\n') {
+    //     // Serial.println(receivedMessage);
+    //     // receivedMessage = ""; // Clear for next message
+    //     // } else {
+    //     // receivedMessage += receivedChar;
+    //     // }
+    //     int incomingByte = SensorSerial.read();
+    //     Serial.print("0x"); // Print in hexadecimal format
+    //     Serial.println(incomingByte, HEX);
+    // }
+
+    while (SensorSerial.available() > 0) {
+        char receivedChar = SensorSerial.read();
+
+        if (receivedChar == 0xD) {
+            Serial.println(receivedMessage);  // Print the received message in the Serial monitor
+            receivedMessage = "";  // Reset the received message
+        } else {
+            receivedMessage += receivedChar;  // Append characters to the received message
+        }
     }
-    header = "";
-    client.stop();
-  }
-}
-
-// put function definitions here:
-void initialWifiSetup() {
-
 }
