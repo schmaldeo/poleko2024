426a4c47fa4616d49b8d246a639d0ea8545a7d41
schmatteo <michal.skrzyszowski@hotmail.com>
2024-04-08 15:52:42 +0200

    Add parameters

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -2,71 +2,88 @@
 #include "Sensor.h"
 #include <WiFiManager.h>
 
+// TODO add sensors by mac
+// TODO network discovery
+// TODO http endpoint, json in http and tcp
 
 HardwareSerial SensorSerial(2);
 Sensor sensor(SensorSerial);
 
 void setupSerial(HardwareSerial& sensorSerial, int rxPin, int txPin);
 
+class IPAddressParameter : public WiFiManagerParameter {
+public:
+    IPAddressParameter(const char *id, const char *placeholder, IPAddress address)
+        : WiFiManagerParameter("") {
+        init(id, placeholder, address.toString().c_str(), 16, "", WFM_LABEL_BEFORE);
+    }
+
+    bool getValue(IPAddress &ip) {
+        return ip.fromString(WiFiManagerParameter::getValue());
+    }
+};
+
+class DefaultGatewayParameter : public WiFiManagerParameter {
+public:
+    DefaultGatewayParameter(const char *id, const char *placeholder, IPAddress address)
+        : WiFiManagerParameter("") {
+        init(id, placeholder, address.toString().c_str(), 16, "", WFM_LABEL_BEFORE);
+    }
+
+    bool getValue(IPAddress &ip) {
+        return ip.fromString(WiFiManagerParameter::getValue());
+    }
+};
+
+class SubnetMaskParameter : public WiFiManagerParameter {
+public:
+    SubnetMaskParameter(const char *id, const char *placeholder, IPAddress address)
+        : WiFiManagerParameter("") {
+        init(id, placeholder, address.toString().c_str(), 16, "", WFM_LABEL_BEFORE);
+    }
+
+    bool getValue(IPAddress &ip) {
+        return ip.fromString(WiFiManagerParameter::getValue());
+    }
+};
+
+struct Settings {
+    uint32_t ip;
+    uint32_t gateway;
+    uint32_t subnetMask;
+} settings;
+
 void setup() {
-    setupSerial(SensorSerial, 16, 17);
     WiFiManager wm;
-
+    wm.setCountry("PL");
     wm.resetSettings();
-
+    IPAddress ip(settings.ip);
+    IPAddress gateway(settings.gateway);
+    IPAddress subnetMask(settings.subnetMask);
+    IPAddressParameter ipParam("ip", "IP address", ip);
+    DefaultGatewayParameter gatewayParam("gateway", "Default gateway", gateway);
+    SubnetMaskParameter subnetParam("subnet", "Subnet mask", subnetMask);
+    wm.addParameter(&ipParam);
+    wm.addParameter(&gatewayParam);
+    wm.addParameter(&subnetParam);
+    std::vector<const char*> menu = {"wifi","param","info","restart","exit"};
+    wm.setMenu(menu);
+    wm.autoConnect();
+    setupSerial(SensorSerial, 16, 17);
+    // TODO open setup with boot button
+    // TODO find out what storage this library uses by default and add the IP settings to it
     bool res;
-    // res = wm.autoConnect(); // auto generated AP name from chipid
-    // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
-    res = wm.autoConnect("AutoConnectAP"); // password protected ap
 
     if(!res) {
         Serial.println("Failed to connect");
-        // ESP.restart();
+        wm.reboot();
     } 
     else {
-        //if you get here you have connected to the WiFi    
-        Serial.println("connected...yeey :)");
+        Serial.println("connected");
     }
-//   WiFi.config(localIP, gateway, subnet);
 }
 
 void loop() {
-//   WiFiClient client = server.available();
-
-//   if (client) {
-//     currentTime = millis();
-//     previousTime = currentTime;
-//     String currentLine = "";
-//     while(client.connected()) {
-//       currentTime = millis();
-//       if (client.available()) {
-//           char c = client.read();
-//           header += c;
-//           if (c == '\n') {
-//             if (currentLine.length() == 0) {
-//               client.println("HTTP/1.1 200 OK");
-//               client.println("Content-type:text/html");
-//               client.println("Connection: close");
-//               client.println();
-
-//               if (header.indexOf("GET /lol") >= 0) {
-//                 client.println("<html><body>xdddd</body></html>");
-//               }
-//               client.println();
-//               break;
-//             } else {
-//               currentLine = "";
-//             }
-//           } else if (c != '\r') {
-//             currentLine += c;
-//           }
-//       }
-//     }
-//     header = "";
-//     client.stop();
-//   }
-
-
     delay(2000);
 
 
@@ -89,5 +106,3 @@ void setupSerial(HardwareSerial& sensorSerial, int rxPin, int txPin) {
     Serial.begin(9600);
     sensorSerial.begin(19200, SERIAL_8N1, rxPin, txPin);
 }
-
-
