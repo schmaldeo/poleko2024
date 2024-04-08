3b407fafd5fcced724171ff1c96e4bc9f147cdd8
schmatteo <michal.skrzyszowski@hotmail.com>
2024-04-09 00:43:02 +0200

    Add handling of static IP

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,10 +1,16 @@
 #include <Arduino.h>
 #include "Sensor.h"
 #include <WiFiManager.h>
+#include <WiFi.h>
+#include <Preferences.h>
 
 // TODO add sensors by mac
 // TODO network discovery
+// ^ both might be doable with UDP broadcast
 // TODO http endpoint, json in http and tcp
+
+constexpr byte BOOT_BUTTON_PIN = 0;
+constexpr byte LED_PIN = 2;
 
 HardwareSerial SensorSerial(2);
 Sensor sensor(SensorSerial);
@@ -12,8 +18,22 @@ TCP tcpServer(5505);
 // WiFiManager Wifi;
 
 // TODO change partition table for filesystem shenanigans
+Preferences preferences;
+
+bool prevButtonState = HIGH;
+bool initialWiFiSetupOver = false;
+
+struct IpSettings {
+    IPAddress ip;
+    IPAddress subnetMask;
+    IPAddress defaultGateway;
+};
 
 void setupSerial(HardwareSerial& sensorSerial, int rxPin, int txPin);
+IpSettings getSavedIpSettings(Preferences& preferences);
+void saveIpSettings(Preferences& preferences, IpSettings& settings);
+void setupWiFi();
+void setupIpSetup();
 
 class IPAddressParameter : public WiFiManagerParameter {
 public:
@@ -22,8 +42,15 @@ public:
         init(id, placeholder, address.toString().c_str(), 16, "", WFM_LABEL_BEFORE);
     }
 
-    bool getValue(IPAddress &ip) {
-        return ip.fromString(WiFiManagerParameter::getValue());
+    IPAddress getValue() {
+        IPAddress ip;
+        ip.fromString(WiFiManagerParameter::getValue());
+        return ip;
+    }
+
+    bool isValid() {
+        IPAddress ip;
+        return ip.fromString(WiFiManagerParameter::getValue()) && ip != 0;
     }
 };
 
@@ -34,8 +61,15 @@ public:
         init(id, placeholder, address.toString().c_str(), 16, "", WFM_LABEL_BEFORE);
     }
 
-    bool getValue(IPAddress &ip) {
-        return ip.fromString(WiFiManagerParameter::getValue());
+    IPAddress getValue() {
+        IPAddress ip;
+        ip.fromString(WiFiManagerParameter::getValue());
+        return ip;
+    }
+
+    bool isValid() {
+        IPAddress ip;
+        return ip.fromString(WiFiManagerParameter::getValue()) && ip != 0;
     }
 };
 
@@ -46,33 +80,23 @@ public:
         init(id, placeholder, address.toString().c_str(), 16, "", WFM_LABEL_BEFORE);
     }
 
-    bool getValue(IPAddress &ip) {
-        return ip.fromString(WiFiManagerParameter::getValue());
+    IPAddress getValue() {
+        IPAddress ip;
+        ip.fromString(WiFiManagerParameter::getValue());
+        return ip;
+    }
+
+    bool isValid() {
+        IPAddress ip;
+        return ip.fromString(WiFiManagerParameter::getValue()) && ip != 0;
     }
 };
 
-struct Settings {
-    uint32_t ip;
-    uint32_t gateway;
-    uint32_t subnetMask;
-} settings;
-
 void setup() {
-    WiFiManager wm;
-    wm.setCountry("PL");
-    wm.resetSettings();
-    IPAddress ip(settings.ip);
-    IPAddress gateway(settings.gateway);
-    IPAddress subnetMask(settings.subnetMask);
-    IPAddressParameter ipParam("ip", "IP address", ip);
-    DefaultGatewayParameter gatewayParam("gateway", "Default gateway", gateway);
-    SubnetMaskParameter subnetParam("subnet", "Subnet mask", subnetMask);
-    wm.addParameter(&ipParam);
-    wm.addParameter(&gatewayParam);
-    wm.addParameter(&subnetParam);
-    std::vector<const char*> menu = {"wifi","param","info","restart","exit"};
-    wm.setMenu(menu);
-    wm.autoConnect();
+    Preferences preferences;
+    auto ipSettings = getSavedIpSettings(preferences);
+    WiFi.config(ipSettings.ip, ipSettings.defaultGateway, ipSettings.subnetMask);
+    pinMode(LED_PIN, OUTPUT);
     setupSerial(SensorSerial, 16, 17);
     // TODO open setup with boot button
     // TODO find out what storage this library uses by default and add the IP settings to it
@@ -90,42 +114,21 @@ void setup() {
 }
 
 void loop() {
-//   WiFiClient client = server.available();
+    auto buttonState = digitalRead(BOOT_BUTTON_PIN);
+    if (buttonState == LOW && prevButtonState == HIGH) {
+        setupIpSetup();
+    }
+    prevButtonState = buttonState;
+    // TODO test if this works properly connection lost
+    if (initialWiFiSetupOver == true && WiFi.status() != WL_CONNECTED) {
+        digitalWrite(LED_PIN, LOW);
+        setupIpSetup();
+    }
 
-//   if (client) {
-//     currentTime = millis();
-//     previousTime = currentTime;
-//     String currentLine = "";
-//     while(client.connected()) {
-//       currentTime = millis();
-//       if (client.available()) {
-//           char c = client.read();
-//           header += c;
-//           if (c == '\n') {
-//             if (currentLine.length() == 0) {
-//               client.println("HTTP/1.1 200 OK");
-//               client.println("Content-type:text/html");
-//               client.println("Connection: close");
-//               client.println();
+    if (WiFi.status() == WL_CONNECTION_LOST) {
+        // TODO start blinking
 
-//               if (header.indexOf("GET /lol") >= 0) {
-//                 client.println("<html><body>xdddd</body></html>");
-//               }
-//               client.println();
-//               break;
-//             } else {
-//               currentLine = "";
-//             }
-//           } else if (c != '\r') {
-//             currentLine += c;
-//           }
-//       }
-//     }
-//     header = "";
-//     client.stop();
-//   }
-
-
+    }
     // delay(2000);
 
 
@@ -149,4 +152,71 @@ void setupSerial(HardwareSerial& sensorSerial, int rxPin, int txPin) {
     Serial.begin(9600);
     sensorSerial.begin(19200, SERIAL_8N1, rxPin, txPin);
     sensorSerial.print("{F99RDD\r\n}");
+}
+
+void setupWiFi() {
+}
+
+void setupIpSetup() {
+    Preferences preferences;
+    auto prefSettings = getSavedIpSettings(preferences);
+
+    // reset static IP in case user wants to switch to another network
+    WiFi.config(0u, 0u, 0u);
+    WiFiManager wm;
+    wm.setCountry("PL");
+    IPAddressParameter ipParam("ip", "IP address", prefSettings.ip);
+    SubnetMaskParameter maskParam("subnet", "Subnet mask", prefSettings.subnetMask);
+    DefaultGatewayParameter gatewayParam("gateway", "Default gateway", prefSettings.defaultGateway);
+    wm.addParameter(&ipParam);
+    wm.addParameter(&maskParam);
+    wm.addParameter(&gatewayParam);
+    std::vector<const char*> menu = {"wifi","param","info","restart","exit"};
+    wm.setMenu(menu);
+    bool connectedOrChangedWiFi = wm.startConfigPortal();
+
+    // if the user used the portal to connect to connect to a different WiFi AP, set DHCP
+    if (connectedOrChangedWiFi) {
+        auto dhcpSettings = IpSettings{0u, 0u, 0u};
+        saveIpSettings(preferences, dhcpSettings);
+        return;
+    }
+
+    // check if:
+    // 1. all are valid
+    // 2. at least one is different from the one stored
+    // then WiFi.config and preferences.put
+    if (ipParam.isValid() && gatewayParam.isValid() && maskParam.isValid()) {
+        IPAddress paramIp = ipParam.getValue();
+        IPAddress paramGateway = gatewayParam.getValue();
+        IPAddress paramMask = maskParam.getValue();
+        if ((prefSettings.ip != paramIp) || (prefSettings.defaultGateway != paramGateway) || (prefSettings.subnetMask != paramMask)) {
+            auto settings = IpSettings{paramIp, paramGateway, paramMask};
+            saveIpSettings(preferences, settings);
+            // FIXME doesnt persistently set static
+            // wm.setSTAStaticIPConfig(paramIp, paramGateway, paramMask);
+            WiFi.config(paramIp, paramGateway, paramMask);
+        }
+    }
+}
+
+IpSettings getSavedIpSettings(Preferences& preferences) {
+    preferences.begin("ipSettings");
+    auto prefIp = preferences.getString("ip", "");
+    IPAddress ip;
+    ip.fromString(prefIp);
+    auto prefMask = preferences.getString("mask", "");
+    IPAddress mask;
+    mask.fromString(prefMask);
+    auto prefGateway = preferences.getString("gateway", "");
+    IPAddress gateway;
+    gateway.fromString(prefGateway);
+    return IpSettings{ip, mask, gateway};
+}
+
+void saveIpSettings(Preferences& preferences, IpSettings& settings) {
+    preferences.begin("ipSettings");
+    preferences.putString("ip", settings.ip.toString());
+    preferences.putString("mask", settings.subnetMask.toString());
+    preferences.putString("gateway", settings.defaultGateway.toString());
 }
