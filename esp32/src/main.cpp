7d8a8cc1d2babe20226c41a3950a92f3bbebe6ad
schmatteo <michal.skrzyszowski@hotmail.com>
2024-04-08 15:52:42 +0200

    Add parameters

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -2,6 +2,9 @@
 #include "Sensor.h"
 #include <WiFiManager.h>
 
+// TODO add sensors by mac
+// TODO network discovery
+// TODO http endpoint, json in http and tcp
 
 HardwareSerial SensorSerial(2);
 Sensor sensor(SensorSerial);
@@ -12,30 +15,75 @@ TCP tcpServer(5505);
 
 void setupSerial(HardwareSerial& sensorSerial, int rxPin, int txPin);
 
-void setup() {
-    WiFi.mode(WIFI_STA);
-    WiFi.config(IPAddress(192, 168, 11, 20), IPAddress(192, 168, 11, 1), IPAddress(255, 255, 255, 0));
-    WiFi.begin("TP-Link_DF70", "74884728");
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
+class IPAddressParameter : public WiFiManagerParameter {
+public:
+    IPAddressParameter(const char *id, const char *placeholder, IPAddress address)
+        : WiFiManagerParameter("") {
+        init(id, placeholder, address.toString().c_str(), 16, "", WFM_LABEL_BEFORE);
     }
-    setupSerial(SensorSerial, 16, 17);
+
+    bool getValue(IPAddress &ip) {
+        return ip.fromString(WiFiManagerParameter::getValue());
+    }
+};
+
+class DefaultGatewayParameter : public WiFiManagerParameter {
+public:
+    DefaultGatewayParameter(const char *id, const char *placeholder, IPAddress address)
+        : WiFiManagerParameter("") {
+        init(id, placeholder, address.toString().c_str(), 16, "", WFM_LABEL_BEFORE);
+    }
+
+    bool getValue(IPAddress &ip) {
+        return ip.fromString(WiFiManagerParameter::getValue());
+    }
+};
+
+class SubnetMaskParameter : public WiFiManagerParameter {
+public:
+    SubnetMaskParameter(const char *id, const char *placeholder, IPAddress address)
+        : WiFiManagerParameter("") {
+        init(id, placeholder, address.toString().c_str(), 16, "", WFM_LABEL_BEFORE);
+    }
+
+    bool getValue(IPAddress &ip) {
+        return ip.fromString(WiFiManagerParameter::getValue());
+    }
+};
+
+struct Settings {
+    uint32_t ip;
+    uint32_t gateway;
+    uint32_t subnetMask;
+} settings;
+
+void setup() {
     WiFiManager wm;
-
+    wm.setCountry("PL");
     wm.resetSettings();
-
+    IPAddress ip(settings.ip);
+    IPAddress gateway(settings.gateway);
+    IPAddress subnetMask(settings.subnetMask);
+    IPAddressParameter ipParam("ip", "IP address", ip);
+    DefaultGatewayParameter gatewayParam("gateway", "Default gateway", gateway);
+    SubnetMaskParameter subnetParam("subnet", "Subnet mask", subnetMask);
+    wm.addParameter(&ipParam);
+    wm.addParameter(&gatewayParam);
+    wm.addParameter(&subnetParam);
+    std::vector<const char*> menu = {"wifi","param","info","restart","exit"};
+    wm.setMenu(menu);
+    wm.autoConnect();
+    setupSerial(SensorSerial, 16, 17);
+    // TODO open setup with boot button
+    // TODO find out what storage this library uses by default and add the IP settings to it
     bool res;
-    // res = wm.autoConnect(); // auto generated AP name from chipid
-    // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
-    res = wm.autoConnect("AutoConnectAP"); // password protected ap
 
     if(!res) {
         Serial.println("Failed to connect");
-        // ESP.restart();
+        wm.reboot();
     } 
     else {
-        //if you get here you have connected to the WiFi    
-        Serial.println("connected...yeey :)");
+        Serial.println("connected");
     }
 //   WiFi.config(localIP, gateway, subnet);
     tcpServer.setup(sensor);
