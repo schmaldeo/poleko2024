68d09db48ed80c1999bbe0266c507c8d24a8293f
schmatteo <michal.skrzyszowski@hotmail.com>
2024-04-14 14:53:08 +0200

    Add Json attributes and overrides for Sensor class in DbContext

--- PolekoWebApp/PolekoWebApp/Data/ApplicationDbContext.cs
+++ PolekoWebApp/PolekoWebApp/Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,7 +12,36 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
 
 public class Sensor
 {
+    [JsonIgnore]
     public int SensorId { get; set; }
+    
+    [JsonPropertyName("ip")]
     public string? IpAddress { get; set; }
+    
+    [JsonPropertyName("mac")]
     public string? MacAddress { get; set; }
+
+    public static bool operator ==(Sensor a, Sensor b)
+    {
+        return a.IpAddress == b.IpAddress && a.MacAddress == b.MacAddress;
+    }
+    
+    public static bool operator !=(Sensor a, Sensor b)
+    {
+        return a.IpAddress != b.IpAddress || a.MacAddress != b.MacAddress;
+    }
+    
+    public override bool Equals(object? obj)
+    {
+        if (obj is Sensor other)
+        {
+            return IpAddress == other.IpAddress && MacAddress == other.MacAddress;
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(IpAddress, MacAddress);
+    }
 }
