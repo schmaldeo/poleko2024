f1bf38c9960be3390bef810f07dc3ad080d26f50
schmatteo <michal.skrzyszowski@hotmail.com>
2024-04-15 12:35:14 +0200

    Read sensor data when entering device

---
+++ PolekoWebApp/PolekoWebApp/Data/SensorData.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PolekoWebApp.Data;
+
+public class SensorData
+{
+    [JsonPropertyName("humidity")]
+    public float Humidity { get; set; }
+    [JsonPropertyName("temperature")]
+    public float Temperature { get; set; }
+    [JsonPropertyName("rssi")]
+    public int Rssi { get; set; }
+}
