840dd18111f05126efb3514d59e94c29e951cf14
schmatteo <michal.skrzyszowski@hotmail.com>
2024-04-17 16:36:24 +0200

    Add annotations in SensorData

--- PolekoWebApp/PolekoWebApp/Data/SensorData.cs
+++ PolekoWebApp/PolekoWebApp/Data/SensorData.cs
@@ -1,14 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
 
 namespace PolekoWebApp.Data;
 
+[PrimaryKey(nameof(Epoch), nameof(SensorId))]
 public class SensorData
 {
     [JsonPropertyName("humidity")] public float Humidity { get; set; }
     [JsonPropertyName("temperature")] public float Temperature { get; set; }
     [NotMapped] [JsonPropertyName("rssi")] public int Rssi { get; set; }
     [JsonIgnore] public long Epoch { get; set; }
-    [JsonIgnore] public int SensorId { get; set; }
+    [ForeignKey(nameof(Sensor))] [Required] [JsonIgnore] public int SensorId { get; set; }
     [JsonIgnore] public Sensor Sensor { get; set; }
 }
